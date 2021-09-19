f36a8a4ec5aeb8a7827f50b784a6f4d1de1e90d3
sonalsithara2352 <sonalsithara2352@gmail.com>
2021-09-20 01:38:20 +0530

    Add Category With Function

--- src/pages/theme.js
+++ src/pages/theme.js
@@ -1,130 +1,224 @@
-import { Helmet } from 'react-helmet';
-import { Box, Card, Container, Grid, Link, Typography } from '@material-ui/core';
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import {
+  Box,
+  Grid,
+  TextField,
+  Button,
+  Typography,
+  Backdrop,
+  CircularProgress,
+  Snackbar,
+  Alert,
+} from "@material-ui/core";
 
-export const Theme = () => (
-  <>
-    <Helmet>
-      <title>Theme | Carpatin Dashboard</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        pb: 3,
-        pt: 8
-      }}
-    >
-      <Container maxWidth="lg">
-        <Typography
-          color="textPrimary"
-          sx={{ mb: 3 }}
-          variant="h4"
-        >
-          Typography
-        </Typography>
+import { db, storage } from "../Firebase";
+
+export const Theme = () => {
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryFileName, setCategoryFileName] = useState(
+    "Upload Category Image"
+  );
+  const [categoryFile, setCategoryFile] = useState([]);
+
+  //Validation//
+  const [categoryNameError, setCategoryNameError] = useState(false);
+  const [categoryFileNameError, setCategoryFileNameError] = useState(false);
+
+  const [progress, setProgress] = useState(0);
+  const [backdrop, setBackdrop] = useState(false);
+
+  const [state, setState] = useState({
+    open: false,
+    vertical: "top",
+    horizontal: "center",
+  });
+  const { vertical, horizontal, open } = state;
+  const [successMessage, setSuccessMessage] = useState("");
+
+  function uploadFile(file) {
+    if (file.target.files[0]) {
+      setCategoryFileName(file.target.files[0].name);
+      setCategoryFile(file.target.files[0]);
+    }
+  }
+
+  function addCategory() {
+    if (categoryName === "") {
+      setCategoryNameError(true);
+    } else {
+      setCategoryNameError(false);
+    }
+    if (categoryFileName === "Upload Category Image") {
+      setCategoryFileNameError(true);
+    } else {
+      if (categoryFile.type.startsWith("image")) {
+        setCategoryFileNameError(false);
+      } else {
+        setCategoryFileNameError(true);
+      }
+    }
+
+    if (categoryNameError === false && categoryFileNameError === false) {
+      const uploadImage = storage
+        .ref(`Category_Images/${categoryFile.name}`)
+        .put(categoryFile);
+
+      uploadImage.on(
+        "state_changed",
+        (snapshot) => {
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setBackdrop(true);
+          setProgress(prog);
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("Category_Images")
+            .child(categoryFile.name)
+            .getDownloadURL()
+            .then((url) => {
+              setBackdrop(false);
+              db.collection("GoBook")
+                .doc(categoryName)
+                .set({
+                  name: categoryName,
+                  image_url: url,
+                })
+                .then(() => {
+                  setSuccessMessage("Category Added");
+                  openMessage({
+                    vertical: "top",
+                    horizontal: "center",
+                  });
+                });
+            });
+        }
+      );
+    }
+  }
+
+  function openMessage(newState) {
+    setState({ open: true, ...newState });
+  }
+
+  function closeMessage() {
+    setState({ ...state, open: false });
+  }
+  return (
+    <>
+      <Helmet>
+        <title>GoBooks Category</title>
+      </Helmet>
+      {/* //! Snackbat// */}
+      <Snackbar
+        key={vertical + horizontal}
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        onClose={closeMessage}
+        autoHideDuration={5000}
+      >
+        <Alert severity="success" onClose={closeMessage} sx={{ width: "100%" }}>
+          {successMessage}
+        </Alert>
+      </Snackbar>
+      {/* //! Backdrop// */}
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backdrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Box
+        sx={{
+          backgroundColor: "Background.default",
+          height: "100%",
+        }}
+        p={2}
+      >
         <Grid
+          flex
+          justifyContent="center"
+          alignItems="center"
+          sx={{ height: "100%" }}
           container
-          spacing={3}
         >
-          <Grid
-            item
-            md={4}
-            xs={12}
-          >
-            <Typography
-              color="textPrimary"
-              sx={{ mb: 1 }}
-              variant="h6"
+          <Grid item xs={12} md={6}>
+            <Box
+              m={8}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
+                // alignItems: "center",
+                border: "1px solid gray",
+                borderRadius: "10px",
+              }}
+              px={6}
+              py={6}
             >
-              Headings
-            </Typography>
-            <Typography
-              color="textSecondary"
-              variant="body2"
-            >
-              We use
-              {' '}
-              <Link
-                color="primary"
-                href="https://fonts.google.com/specimen/Inter"
-                target="_blank"
-                variant="inherit"
+              <Typography
+                variant="h4"
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginBottom: "30px",
+                }}
               >
-                Inter
-              </Link>
-              {' '}
-              as our main font name for all our components.
-              <br />
-              <br />
-              The sizes have been reconfigured to match the needs of a clean
-              and SEO friendly dashboard.
-            </Typography>
+                Add Category
+              </Typography>
+              <TextField
+                fullWidth
+                label="Category Name"
+                variant="outlined"
+                sx={{ marginBottom: "16px" }}
+                onChange={(val) => {
+                  setCategoryName(val.target.value);
+                }}
+                value={categoryName}
+                error={categoryNameError}
+                helperText={
+                  categoryNameError ? "Please Fill Category Name" : null
+                }
+              />
+              <Button
+                variant="outlined"
+                component="label"
+                color={categoryFileNameError ? "error" : "primary"}
+              >
+                {categoryFileName}
+                <input type="file" hidden onChange={uploadFile} />
+              </Button>
+              <Typography
+                sx={{
+                  fontSize: "12px",
+                  color: "red",
+                  display: categoryFileNameError ? "flex" : "none",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                Please Add Image
+              </Typography>
+              <Button
+                sx={{ marginTop: "16px" }}
+                variant="contained"
+                onClick={addCategory}
+              >
+                Add Category
+              </Button>
+            </Box>
           </Grid>
-          <Grid
-            item
-            md={8}
-            xs={12}
-          >
-            <Card
-              sx={{
-                display: 'grid',
-                gap: 3,
-                gridAutoFlow: 'row',
-                p: 3
-              }}
-              variant="outlined"
-            >
-              {['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map((variant) => (
-                <Typography
-                  color="textPrimary"
-                  key={variant}
-                  variant={variant}
-                >
-                  Heading
-                </Typography>
-              ))}
-            </Card>
-          </Grid>
-          <Grid
-            item
-            md={4}
-            xs={12}
-          >
-            <Typography
-              color="textPrimary"
-              sx={{ mb: 1 }}
-              variant="h6"
-            >
-              Body
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            md={8}
-            xs={12}
-          >
-            <Card
-              sx={{
-                display: 'grid',
-                gap: 3,
-                gridAutoFlow: 'row',
-                p: 3
-              }}
-              variant="outlined"
-            >
-              {['body1', 'body2', 'subtitle1', 'subtitle2', 'overline', 'caption'].map((variant) => (
-                <Typography
-                  color="textPrimary"
-                  key={variant}
-                  sx={{ textTransform: variant !== 'overline' && 'capitalize' }}
-                  variant={variant}
-                >
-                  {variant}
-                </Typography>
-              ))}
-            </Card>
+          <Grid item xs={12} md={6}>
+            <h4>sd</h4>
           </Grid>
         </Grid>
-      </Container>
-    </Box>
-  </>
-);
+      </Box>
+    </>
+  );
+};
