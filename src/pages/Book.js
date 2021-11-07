7b269429340dea1c26250221a26eec5a531aee69
sonal <sonalsithara2352@gmail.com>
2021-11-07 17:53:23 +0530

    DB Changes

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -2,8 +2,8 @@
  * Copyright (c)  2021-2021, Sonal Sithara
  */
 
-import { useEffect, useRef, useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { useEffect, useRef, useState } from "react";
+import { Helmet } from "react-helmet";
 import {
   Alert,
   Backdrop,
@@ -17,20 +17,20 @@ import {
   Select,
   Snackbar,
   TextField,
-  Typography
-} from '@material-ui/core';
+  Typography,
+} from "@material-ui/core";
 
-import { db, storage } from '../Firebase';
+import { db, storage } from "../Firebase";
 
 export const Book = () => {
-  const [bookName, setbookName] = useState('');
-  const [bookPrice, setbookPrice] = useState('');
-  const [bookQty, setbookQty] = useState('');
-  const [categoryValue, setcategoryValue] = useState('');
-  const [gradeValue, setgradeValue] = useState('');
+  const [bookName, setbookName] = useState("");
+  const [bookPrice, setbookPrice] = useState("");
+  const [bookQty, setbookQty] = useState("");
+  const [categoryValue, setcategoryValue] = useState("");
+  const [gradeValue, setgradeValue] = useState("");
   const [grades, setgrades] = useState([]);
   const [categoryName, setcategoryName] = useState([]);
-  const [bookFileName, setbookFileName] = useState('Upload Book Image');
+  const [bookFileName, setbookFileName] = useState("Upload Book Image");
   const [bookFile, setbookFile] = useState([]);
   const noneGrade = useRef();
   //Validation//
@@ -47,14 +47,14 @@ export const Book = () => {
 
   const [state, setState] = useState({
     open: false,
-    vertical: 'top',
-    horizontal: 'center'
+    vertical: "top",
+    horizontal: "center",
   });
   const { vertical, horizontal, open } = state;
-  const [successMessage, setSuccessMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
 
   useEffect(() => {
-    db.collection('GoBook').onSnapshot((snapshot) => {
+    db.collection("GoBook").onSnapshot((snapshot) => {
       setcategoryName(snapshot.docs.map((doc) => doc.id));
     });
   }, []);
@@ -67,23 +67,23 @@ export const Book = () => {
   }
 
   function validation() {
-    if (categoryValue === '') {
+    if (categoryValue === "") {
       setcategoryError(true);
-    } else if (categoryValue.startsWith('Grade') && gradeValue === '') {
+    } else if (categoryValue.startsWith("Grade") && gradeValue === "") {
       setcategoryError(false);
       setgradeError(true);
-    } else if (bookName === '') {
+    } else if (bookName === "") {
       setgradeError(false);
       setbookNameError(true);
-    } else if (bookPrice === '') {
+    } else if (bookPrice === "") {
       setbookNameError(false);
       setpriceError(true);
-    } else if (bookQty === '') {
+    } else if (bookQty === "") {
       setpriceError(false);
       setqtyError(true);
     } else if (
-      bookFileName === 'Upload Book Image' ||
-      !bookFile.type.startsWith('image')
+      bookFileName === "Upload Book Image" ||
+      !bookFile.type.startsWith("image")
     ) {
       setqtyError(false);
       setbookFileNameError(true);
@@ -93,6 +93,42 @@ export const Book = () => {
     }
   }
 
+  // .collection(
+  //       categoryValue.startsWith('Grade')
+  //           ? gradeValue
+  //           : noneGrade.current.value
+  //   )
+
+  function addBookData(url) {
+    let collectionReference;
+    let documentReference = db.collection("GoBook").doc(categoryValue);
+    if (categoryValue.startsWith("Grade")) {
+      collectionReference = documentReference
+        .collection(gradeValue)
+        .doc(`${gradeValue}_Books`)
+        .collection("Books");
+    } else {
+      collectionReference = documentReference.collection("Books");
+    }
+    collectionReference
+      .doc(bookName)
+      .set({
+        bookimage: url,
+        bookname: bookName,
+        bookprice: bookPrice,
+        qty: bookQty,
+        date: new Date().toJSON().slice(0, 10).replace(/-/g, "/"),
+      })
+      .then(() => {
+        setSuccessMessage("Book Added");
+        openMessage({
+          vertical: "top",
+          horizontal: "center",
+        });
+        clean();
+      });
+  }
+
   function addBook() {
     if (validation()) {
       const uploadImage = storage
@@ -100,7 +136,7 @@ export const Book = () => {
         .put(bookFile);
 
       uploadImage.on(
-        'state_changed',
+        "state_changed",
         (snapshot) => {
           const prog = Math.round(
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
@@ -113,50 +149,29 @@ export const Book = () => {
         },
         () => {
           storage
-            .ref('Book_Images')
+            .ref("Book_Images")
             .child(bookFile.name)
             .getDownloadURL()
             .then((url) => {
               setBackdrop(false);
-              db.collection('GoBook')
-                .doc(categoryValue)
-                .collection(
-                  categoryValue.startsWith('Grade')
-                    ? gradeValue
-                    : noneGrade.current.value
-                )
-                .doc(bookName)
-                .set({
-                  bookimage: url,
-                  bookname: bookName,
-                  bookprice: bookPrice,
-                  qty: bookQty,
-                  date:new Date().toJSON().slice(0,10).replace(/-/g,'/'),
-                })
-                .then(() => {
-                  setSuccessMessage('Book Added');
-                  openMessage({
-                    vertical: 'top',
-                    horizontal: 'center'
-                  });
-                  clean();
-                });
-            }).catch((erro) => {
-            console.log(erro);
-          });
+              addBookData(url)
+            })
+            .catch((erro) => {
+              console.log(erro);
+            });
         }
       );
     }
   }
 
   function clean() {
-    setbookFileName('Upload Book Image');
-    setbookName('');
+    setbookFileName("Upload Book Image");
+    setbookName("");
     setbookFile([]);
-    setbookPrice('');
-    setbookQty('');
-    setgradeValue('');
-    setcategoryValue('');
+    setbookPrice("");
+    setbookQty("");
+    setgradeValue("");
+    setcategoryValue("");
     setcategoryError(false);
     setgradeError(false);
     setbookNameError(false);
@@ -174,13 +189,13 @@ export const Book = () => {
   }
 
   function gradeDisable(catename) {
-    if (catename.startsWith('Grade 1')) {
+    if (catename.startsWith("Grade 1")) {
       setgrades([]);
       setgradeDisableCkeck(false);
       for (let index = 1; index <= 5; index++) {
         setgrades((oldArray) => [...oldArray, `Grade_${index}`]);
       }
-    } else if (catename.startsWith('Grade 6')) {
+    } else if (catename.startsWith("Grade 6")) {
       setgrades([]);
       setgradeDisableCkeck(false);
       for (let index = 6; index <= 10; index++) {
@@ -205,42 +220,42 @@ export const Book = () => {
         onClose={closeMessage}
         autoHideDuration={5000}
       >
-        <Alert severity="success" onClose={closeMessage} sx={{ width: '100%' }}>
+        <Alert severity="success" onClose={closeMessage} sx={{ width: "100%" }}>
           {successMessage}
         </Alert>
       </Snackbar>
 
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={backdrop}
       >
-        <CircularProgress color="inherit"/>
+        <CircularProgress color="inherit" />
       </Backdrop>
       {/* ////////////? UTIL END///////// */}
 
       <Box
         sx={{
-          backgroundColor: 'Background.default',
-          height: '100%'
+          backgroundColor: "Background.default",
+          height: "100%",
         }}
       >
         <Grid
           flex
           justifyContent="center"
           alignItems="center"
-          sx={{ height: '100%' }}
+          sx={{ height: "100%" }}
           container
         >
           <Grid item xs={12} md={6}>
             <Box
               mx={8}
               sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-around',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
                 // alignItems: "center",
-                border: '1px solid gray',
-                borderRadius: '10px'
+                border: "1px solid gray",
+                borderRadius: "10px",
               }}
               px={5}
               py={5}
@@ -248,15 +263,15 @@ export const Book = () => {
               <Typography
                 variant="h4"
                 sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: '30px'
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginBottom: "30px",
                 }}
               >
                 Add Book
               </Typography>
-              <FormControl size="small" sx={{ marginBottom: '16px' }} fullWidth>
+              <FormControl size="small" sx={{ marginBottom: "16px" }} fullWidth>
                 <InputLabel id="demo-simple-select-label">
                   Select Category
                 </InputLabel>
@@ -278,8 +293,8 @@ export const Book = () => {
               {gradeDisableCkeck ? (
                 <TextField
                   inputRef={noneGrade}
-                  sx={{ marginBottom: '16px' }}
-                  value={'Books'}
+                  sx={{ marginBottom: "16px" }}
+                  value={"Books"}
                   disabled
                   size="small"
                 />
@@ -287,7 +302,7 @@ export const Book = () => {
                 <FormControl
                   size="small"
                   disabled={gradeDisableCkeck}
-                  sx={{ marginBottom: '16px' }}
+                  sx={{ marginBottom: "16px" }}
                   fullWidth
                 >
                   <InputLabel id="demo-simple-select-label-grade">
@@ -313,13 +328,13 @@ export const Book = () => {
                 fullWidth
                 label="Book Name"
                 variant="outlined"
-                sx={{ marginBottom: '16px' }}
+                sx={{ marginBottom: "16px" }}
                 onChange={(val) => {
                   setbookName(val.target.value);
                 }}
                 value={bookName}
                 error={bookNameError}
-                helperText={bookNameError ? 'Please Fill Book Name' : null}
+                helperText={bookNameError ? "Please Fill Book Name" : null}
               />
               <TextField
                 type="number"
@@ -327,13 +342,13 @@ export const Book = () => {
                 fullWidth
                 label="Book Price"
                 variant="outlined"
-                sx={{ marginBottom: '16px' }}
+                sx={{ marginBottom: "16px" }}
                 onChange={(val) => {
                   setbookPrice(val.target.value);
                 }}
                 value={bookPrice}
                 error={priceError}
-                helperText={priceError ? 'Please Fill Book Price' : null}
+                helperText={priceError ? "Please Fill Book Price" : null}
               />
               <TextField
                 type="number"
@@ -341,35 +356,35 @@ export const Book = () => {
                 fullWidth
                 label="Book Quantity"
                 variant="outlined"
-                sx={{ marginBottom: '16px' }}
+                sx={{ marginBottom: "16px" }}
                 onChange={(val) => {
                   setbookQty(val.target.value);
                 }}
                 value={bookQty}
                 error={qtyError}
-                helperText={qtyError ? 'Please Fill Book Quantity' : null}
+                helperText={qtyError ? "Please Fill Book Quantity" : null}
               />
               <Button
                 variant="outlined"
                 component="label"
-                color={bookFileNameError ? 'error' : 'primary'}
+                color={bookFileNameError ? "error" : "primary"}
               >
                 {bookFileName}
-                <input type="file" hidden onChange={uploadFile}/>
+                <input type="file" hidden onChange={uploadFile} />
               </Button>
               <Typography
                 sx={{
-                  fontSize: '12px',
-                  color: 'red',
-                  display: bookFileNameError ? 'flex' : 'none',
-                  justifyContent: 'center',
-                  alignItems: 'center'
+                  fontSize: "12px",
+                  color: "red",
+                  display: bookFileNameError ? "flex" : "none",
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
               >
                 Please Add Image
               </Typography>
               <Button
-                sx={{ marginTop: '16px' }}
+                sx={{ marginTop: "16px" }}
                 variant="contained"
                 onClick={addBook}
               >
